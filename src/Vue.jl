d1268f3d0f909a433feb3b60e21db1cf14c31772
Shashi Gowda <shashi@juliacomputing.com>
2018-02-21 21:27:20 +0530

    revert making all params Observables

--- src/Vue.jl
+++ src/Vue.jl
@@ -36,30 +36,28 @@ function vue(template, data=Dict(); kwargs...)
     watches = Dict()
     for (k, v) in data
         skey = string(k)
-        if !isa(v, Observable)
-            v = Observable{Any}(v)
-        end
+        if isa(v, Observable)
+            # associate the observable with the widget
+            setobservable!(widget, skey, v)
 
-        # associate the observable with the widget
-        setobservable!(widget, skey, v)
-
-        # forward updates from Julia to the Vue property, Vue watches the
-        # this.vue object's data properties, and updates its view when
-        # they change
-        onjs(v, @js function (val)
-            # This copy is needed to avoid the Vue.js reactivity system
-            @var valcopy = JSON.parse(JSON.stringify(val))
-            this.vue[$skey] = valcopy
-        end)
-
-        # Forward vue updates back to WebIO observable, which will send it
-        # to Julia iff the the observable's sync property is true
-        watches[skey] =
-            @js this.vue["\$watch"]($skey, function (val, oldval)
-                # This copy is needed to avoid Vue.js reactivity system
+            # forward updates from Julia to the Vue property, Vue watches the
+            # this.vue object's data properties, and updates its view when
+            # they change
+            onjs(v, @js function (val)
+                # This copy is needed to avoid the Vue.js reactivity system
                 @var valcopy = JSON.parse(JSON.stringify(val))
-                $v[] = valcopy
+                this.vue[$skey] = valcopy
             end)
+
+            # Forward vue updates back to WebIO observable, which will send it
+            # to Julia iff the the observable's sync property is true
+            watches[skey] =
+                @js this.vue["\$watch"]($skey, function (val, oldval)
+                    # This copy is needed to avoid Vue.js reactivity system
+                    @var valcopy = JSON.parse(JSON.stringify(val))
+                    $v[] = valcopy
+                end)
+        end
     end
 
     ondeps_fn = @js function (Vue)
